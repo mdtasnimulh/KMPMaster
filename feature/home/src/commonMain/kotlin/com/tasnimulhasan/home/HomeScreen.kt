d1a5d1ae1cca00b059319660f1620ec5fbcfc0c1
Md. Tasnimul Hasan <mdtasnimulh@gmail.com>
2025-07-23 14:11:27 +0600

    data store implementation ongoing

--- feature/home/src/commonMain/kotlin/com/tasnimulhasan/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/com/tasnimulhasan/home/HomeScreen.kt
@@ -8,17 +8,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = koinViewModel()
 ) {
-    val isFirstLaunch = viewModel.isFirstLaunch.collectAsStateWithLifecycle()
+    val isFirstLaunch = viewModel.isFirstLaunch.collectAsState()
 
     Column(
         modifier = Modifier
