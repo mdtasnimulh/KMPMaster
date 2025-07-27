8176d25e6f27d534af375286fde641ff592d6592
Md. Tasnimul Hasan <mdtasnimulh@gmail.com>
2025-07-27 13:56:05 +0600

    start implementing onboarding screen

--- feature/home/src/commonMain/kotlin/com/tasnimulhasan/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/com/tasnimulhasan/home/HomeScreen.kt
@@ -2,9 +2,7 @@ package com.tasnimulhasan.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +10,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -36,17 +33,6 @@ fun HomeScreen(
     ) {
         Text(
             text = "Home Screen !!!",
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-        Text(
-            text = if (isFirstLaunch.value == "Y") {
-                "First Launch ${isFirstLaunch.value}"
-            } else {
-                "Already Launched ${isFirstLaunch.value}"
-            },
             style = MaterialTheme.typography.titleLarge
         )
     }
